72c034eb8608663e51ad6d618e601ac46bca4f07
jitterxx <sergey@reshim.com>
2015-11-03 19:05:01 +0300

    Чтение классифицированных сообщений из базы и вывод на экран.

--- notificater.py
+++ notificater.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python -t
 # coding: utf8
 
-__author__ = 'sergey'
-
 import sqlalchemy
 from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from configuration import *
+import re
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+__author__ = 'sergey'
 
 sql_uri = "mysql://%s:%s@%s/clear_data?charset=utf8" % (db_user, db_pass, db_host)
 
-Base = sqlalchemy.ext.declarative.declarative_base()
+Base = declarative_base()
 Engine = sqlalchemy.create_engine(sql_uri, pool_size=20)
 Session = sqlalchemy.orm.sessionmaker(bind=Engine)
 
@@ -37,18 +38,20 @@ class Msg(Base):
     cc_recipients_name = Column(sqlalchemy.TEXT())
     message_title = Column(sqlalchemy.TEXT())
     message_text = Column(sqlalchemy.TEXT())
-    orig_date = Column(sqlalchemy.DATETIME)
-    create_date = Column(sqlalchemy.DATETIME)
+    orig_date = Column(sqlalchemy.DATETIME())
+    create_date = Column(sqlalchemy.DATETIME())
     isclassified = Column(sqlalchemy.Integer)
     category = Column(sqlalchemy.String(256))
 
 
 def notify():
-    session = sqlalchemy.Session()
+    session = Session()
 
     query = session.query(Msg).filter(Msg.isclassified == 1).all()
     for msg in query:
         print msg.message_id
+        cats = re.split(":", msg.category)
+        print cats
 
     session.close()
 
