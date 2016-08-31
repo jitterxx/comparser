127fcfba846ad49e38ea38842844b161efd387f7
jitterxx <sergey@reshim.com>
2016-08-31 12:29:24 +0300

    Новая ветка для работы с сайтом - site. Новая версия лендинга.

--- site/web_site.py
+++ site/web_site.py
@@ -75,91 +75,28 @@ class Root(object):
 
         return tmpl.render(user_agent=user_agent, ads_code=ads)
 
-    """
     @cherrypy.expose
-    def promo(self, qr=None):
-        if not qr:
-            name = ""
-            phone = ""
-            mail = ""
-        else:
-            promo_codes = {
-                "PROMOXSW": {"name": "Артур Владимирович", "phone": "+78127024242", "email": ""},
-                "PROMOZAQ": {"name": "Андреас Василис", "phone": "+78124540506", "email": ""},
-                "PROMOCDE": {"name": "Александр Дубовенко", "phone": "+78003331111", "email": ""},
-                "PROMOVFR": {"name": "", "phone": "+78123209620", "email": ""},
-                "PROMOBGT": {"name": "", "phone": " +78126032688", "email": ""},
-                "PROMONHY": {"name": "Владимир Николаевич", "phone": "+78126406021", "email": ""},
-                "PROMOMJU": {"name": "Валерий Сергеевич", "phone": "+78553393604 ", "email": ""},
-                "PROMOPOI": {"name": "Александр Константинович", "phone": "+78124061395", "email": ""}
-            }
+    def test(self, ads=None, utm_source=None, utm_medium=None, utm_campaign=None, utm_term=None):
 
-            if str(qr).upper() in promo_codes.keys():
-                try:
-                    name = promo_codes[str(qr).upper()]["name"]
-                    phone = promo_codes[str(qr).upper()]["phone"]
-                    mail = promo_codes[str(qr).upper()]["email"]
-                except Exception as e:
-                    print "Ошибка при получении данных QR кода. %s" % str(e)
-                    name = ""
-                    phone = ""
-                    mail = ""
-            else:
-                name = ""
-                phone = ""
-                mail = ""
+        tmpl = lookup.get_template("landing_ver7.html")
 
-        tmpl = lookup.get_template("promo_landing_ver3.html")
-
-        return tmpl.render(mail=mail, name=name, phone=phone)
-
-    @cherrypy.expose
-    def send_contacts_demo(self, customer_email=None, customer_name=None, pd=None, ads_code=None):
-        if not customer_email:
-            customer_email = "не указан"
-        if not customer_name:
-            customer_name = "не указано"
+        if not ads:
+            ads = "organic"
+        try:
+            print "Параметры перехода: \n \t ADS: {0}\n\t UTM_SOURCE: {1}\n\t UTM_MEDIUM: {2}\n" \
+                  "\t UTM_CAMPAIGN: {3}\n\t UTM_TERM: {4}\n".format(ads, str(utm_source), str(utm_medium),
+                                                                    str(utm_campaign), str(utm_term))
+        except Exception as e:
+            print "Ошибка при выводе параметров запроса. {}".format(str(e))
 
         try:
-            WSO.landing_customer_contacts(customer_email=customer_email, customer_name=customer_name,
-                                      customer_session=cherrypy.request.headers, pd=pd, ads_code=ads_code)
+            user_agent = parse(cherrypy.request.headers['User-Agent'])
         except Exception as e:
-            print "Ошибка при попытке отправить контакты с лендинга. %s " % str(e)
+            print "Ошибка определения типа клиента. %s" % str(e)
+            user_agent = ""
 
-        print customer_email, customer_name,  cherrypy.request.headers, pd
-        text = "
-        <br>
-        <p class="lead text-left">%s, мы записали email и в ближайшее время свяжемся с Вами.</p>
-        <br>
-        <div class="lead text-left">С уважением,<br> команда Conversation Parser.</div>
-        " % customer_name
+        return tmpl.render(user_agent=user_agent, ads_code=ads)
 
-        return ShowNotification().index(text, "/")
-
-    @cherrypy.expose
-    def send_contacts_promo(self, customer_email=None, customer_name=None, customer_phone=None):
-        if not customer_email:
-            customer_email = "не указан"
-        if not customer_name:
-            customer_name = "не указано"
-        if not customer_phone:
-            customer_phone = "не указано"
-
-        if not customer_email and not customer_phone:
-            raise cherrypy.HTTPRedirect("send_contacts_promo")
-
-        try:
-            WSO.landing_customer_contacts(customer_email=customer_email, customer_name=customer_name,
-                                          customer_session=cherrypy.request.headers, customer_phone=customer_phone)
-        except Exception as e:
-            print "Ошибка при попытке отправить контакты с лендинга. %s " % str(e)
-
-        print customer_email, customer_phone, customer_name, cherrypy.request.headers
-
-        tmpl = lookup.get_template("contacts_ok_landing_ver3.html")
-
-        return tmpl.render()
-    """
     @cherrypy.expose
     def yandex_6e3971fd399cf7fd_html(self):
         return lookup.get_template("yandex_6e3971fd399cf7fd.html").render()
