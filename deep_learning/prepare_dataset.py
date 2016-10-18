cdf1a84e4cc231b64a3f54682876a842a666b55c
jitterxx <sergey@reshim.com>
2016-10-18 12:39:21 +0300

    Скрипт подготовки данных для обучения сети. Обучние проходит на машине с достаточным количеством памяти и ЦП.

--- deep_learning/prepare_dataset.py
+++ deep_learning/prepare_dataset.py
@@ -31,9 +31,11 @@ cats = CPO.GetCategory().keys()
 
 limit = 20
 
+train_data_ready = False
+
 try:
-    resp = session.query(CPO.TrainData.category, func.count(CPO.TrainData.category)).\
-        group_by(CPO.TrainData.category).\
+    resp = session.query(CPO.UserTrainData.category, func.count(CPO.UserTrainData.category)).\
+        group_by(CPO.UserTrainData.category).\
         all()
 except Exception as e:
     print(str(e))
@@ -50,41 +52,93 @@ else:
         print("Недостаточно данных для формирования обучающей выборки. В одной из категорий - {} примеров".format(cat_min))
         exit()
 
-exit()
+    for current_cat in cats:
+        print("Готовим категорию: {}".format(current_cat))
+        try:
+            count = session.query(CPO.UserTrainData).filter(CPO.UserTrainData.category == current_cat).limit(limit).count()
 
+            resp = session.query(CPO.UserTrainData).filter(CPO.UserTrainData.category == current_cat).limit(limit)
+        except Exception as e:
+            print(str(e))
+            train_data_ready = False
+        else:
+            print("{} - сообщений".format(count))
+            CAT_PATH = "{}/{}".format(PATH, current_cat)
 
-for current_cat in cats:
-    print("Готовим категорию: {}".format(current_cat))
-    try:
-        count = session.query(CPO.TrainData).filter(CPO.TrainData.category == current_cat).limit(limit).count()
+            if os.path.exists(CAT_PATH):
+                print("Удаляем каталог и старые данные")
+                shutil.rmtree(CAT_PATH)
 
-        resp = session.query(CPO.TrainData).filter(CPO.TrainData.category == current_cat).limit(limit)
-    except Exception as e:
-        print(str(e))
-    else:
-        print("{} - сообщений".format(count))
-        CAT_PATH = "{}/{}".format(PATH, current_cat)
+            print("Создаем новый каталог: {}".format(CAT_PATH))
+            os.makedirs(CAT_PATH)
+            for one in resp:
+                filename = uuid.uuid4().__str__()
+                f = file("{}/{}".format(CAT_PATH, filename), "w")
+                f.write(one.message_title)
+                f.write("\n\n")
+                f.write(one.message_text)
+                f.write("\n\n")
+                f.close()
 
-        if os.path.exists(CAT_PATH):
-            print("Удаляем каталог и старые данные")
-            shutil.rmtree(CAT_PATH)
-
-        print("Создаем новый каталог: {}".format(CAT_PATH))
-        os.makedirs(CAT_PATH)
-        for one in resp:
-            filename = uuid.uuid4().__str__()
-            f = file("{}/{}".format(CAT_PATH, filename), "w")
-            f.write(one.message_title)
-            f.write("\n\n")
-            f.write(one.message_text)
-            f.write("\n\n")
+            print("Записываем информация о категории в class.nfo")
+            f = file("{}/{}".format(CAT_PATH, "class.nfo"), "w")
+            f.write("Name: {}\n".format(current_cat))
+            f.write("Size: {}\n".format(count))
             f.close()
 
-        print("Записываем информация о категории в class.nfo")
-        f = file("{}/{}".format(CAT_PATH, "class.nfo"), "w")
-        f.write("Name: {}\n".format(current_cat))
-        f.write("Size: {}\n".format(count))
-        f.close()
+            train_data_ready = True
 
-session.close()
+finally:
+    session.close()
+
+
+
+if train_data_ready:
+    print("# Примеров для работы достаточно. Ошибок не обнаружено. \n Копируем данные...")
+    # Копируем данные обучения текущей эпохи в train_data из user_train_data и чистим последнюю.
+    try:
+        # Обновляем эпоху
+        CPO.update_epoch()
+    except Exception as e:
+        print("Ошибка при обновлении эпохи обучения. {}".format(str(e)))
+        raise e
+
+    # переносим записи из user_train_data в train_data
+    session = CPO.Session()
+    for one in resp:
+        new = CPO.TrainData()
+        new.message_id = one.message_id
+        new.sender = one.sender
+        new.sender_name = one.sender_name
+        new.recipients = one.recipients
+        new.recipients_name = one.recipients_name
+        new.cc_recipients = one.cc_recipients
+        new.cc_recipients_name = one.cc_recipients_name
+        new.message_title = one.message_title
+        new.message_text = one.message_text
+        new.orig_date = one.orig_date
+        new.create_date = one.create_date
+        new.category = one.category
+        new.train_epoch = one.train_epoch
+        try:
+            session.add(new)
+            session.commit()
+        except Exception as e:
+            print("Ошибка при копировании данных для обучения. {}".format(str(e)))
+            session.close()
+            raise e
+
+    # чистим user_train_data
+    try:
+        for one in resp:
+            session.delete(one)
+        session.commit()
+    except Exception as e:
+        print("Ошибка при удалении данных пользовательского обучения. {}".format(str(e)))
+        raise e
+    finally:
+        session.close()
+else:
+    print("# Примеров для работы НЕ достаточно. Добавьте недостающие данные.")
+
 
